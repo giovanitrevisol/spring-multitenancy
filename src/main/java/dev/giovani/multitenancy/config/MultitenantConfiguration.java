8aca757cc2107e3a3feb92351813fabdfede443a
giovani <giovanitrevisol93@gmail.com>
2025-01-07 20:38:34 -0300

    multitenancy run project

--- src/main/java/dev/giovani/multitenancy/config/MultitenantConfiguration.java
+++ src/main/java/dev/giovani/multitenancy/config/MultitenantConfiguration.java
@@ -33,16 +33,23 @@ public class MultitenantConfiguration {
 
         for (File propertyFile : files) {
             Properties tenantProperties = new Properties();
-            DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//            DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
             try {
                 tenantProperties.load(new FileInputStream(propertyFile));
                 String tenantId = tenantProperties.getProperty("name");
 
-                dataSourceBuilder.driverClassName(tenantProperties.getProperty("datasource.driver-class-name"));
-                dataSourceBuilder.username(tenantProperties.getProperty("datasource.username"));
-                dataSourceBuilder.password(tenantProperties.getProperty("datasource.password"));
-                dataSourceBuilder.url(tenantProperties.getProperty("datasource.url"));
-                resolvedDataSources.put(tenantId, dataSourceBuilder.build());
+                String driverClassName = tenantProperties.getProperty("driver-class-name");
+                String url = tenantProperties.getProperty("url");
+                String username = tenantProperties.getProperty("username");
+                String password = tenantProperties.getProperty("password");
+
+                HikariDataSource dataSource = new HikariDataSource();
+                dataSource.setDriverClassName(driverClassName);
+                dataSource.setJdbcUrl(url);
+                dataSource.setUsername(username);
+                dataSource.setPassword(password);
+
+                resolvedDataSources.put(tenantId, dataSource);
             } catch (IOException exp) {
                 throw new RuntimeException("Problem in tenant datasource:" + exp);
             }
